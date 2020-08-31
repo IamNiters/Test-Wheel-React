c64c35a8307e32260007c2a9aef154f46bb3b2f8
Niters <niters69@gmail.com>
2020-08-31 11:49:28 +0200

    Added wheel

---
+++ wheely/src/Wheel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./wheelstyle.css";
+
+function Wheel() {
+  return (
+    <div>
+      <ul className="HeroCircle">
+        <li>1📚</li>
+        <li>2🎥</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Wheel;
